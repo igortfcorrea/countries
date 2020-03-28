67bc5385012eedb7188f386ecf35c50b8383daf2
Igor Correa <igortfcorrea@gmail.com>
2020-03-28 16:24:32 -0300

    Dagger2 added

--- app/src/main/java/com/example/countries/view/MainActivity.kt
+++ app/src/main/java/com/example/countries/view/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+           setContentView(R.layout.activity_main)
 
         viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
         viewModel.refresh()
--- app/src/main/java/com/example/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/countries/viewmodel/ListViewModel.kt
@@ -1,17 +1,26 @@
 package com.example.countries.viewmodel
 
+
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.countries.di.DaggerApiComponent
 import com.example.countries.model.CountriesService
 import com.example.countries.model.Country
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
 class ListViewModel : ViewModel() {
 
-    private val countriesService = CountriesService()
+    @Inject
+    lateinit var countriesService: CountriesService
+
+    init {
+        DaggerApiComponent.create().inject(this)
+    }
+
     private val disposable = CompositeDisposable()
 
     val countries = MutableLiveData<List<Country>>()
